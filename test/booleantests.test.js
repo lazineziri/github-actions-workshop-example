42e7b58373797c1d1b8c1a73c8d999995cb8b00b
Drilon Recica <drilonrecica.dev@gmail.com>
2022-08-25 03:09:27 +0200

    [github-actions-workshop-example] Fix broken test

--- test/booleantests.test.js
+++ test/booleantests.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert'); describe('Simple Boolean Test', () => {
     it('should return true', () => {
-        assert.equal(true && false, true);
+        assert.equal(true && false, false);
     });
     it('should return false', () => {
         assert.equal(false || true, true);
